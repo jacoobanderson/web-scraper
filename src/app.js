bfa4139e0ec017f1cbf6e9c1877eca1a69e7164a
jacoobanderson <j00cwa@gmail.com>
2022-01-03 01:04:19 +0100

    Create link scraper

--- src/app.js
+++ src/app.js
@@ -1,1 +1,10 @@
-console.log('test')
+// import { Application } from './application.js'
+import { LinkScraper } from './link-scraper.js'
+
+try {
+  const linkTest = new LinkScraper()
+  const test = await linkTest.getLinks('https://courselab.lnu.se/scraper-site-1')
+  console.log(test)
+} catch (error) {
+  console.error(error.message)
+}
--- src/link-scraper.js
+++ src/link-scraper.js
@@ -1,2 +1,21 @@
 import fetch from 'node-fetch'
 import { JSDOM } from 'jsdom'
+
+export class LinkScraper {
+    async getLinks (url) {
+        // Gets the initial raw data from the chosen URL.
+        const initialData = await this.#getInitialData(url)
+
+        const dom = new JSDOM(initialData)
+
+        // Returns an array with the links by getting all the a elements and then accessing the href on each element.
+        const linkScrape = Array.from(dom.window.document.querySelectorAll('a'))
+        .map(ele => ele.href)
+        return linkScrape
+    }
+
+    async #getInitialData (url) {
+        const response = await fetch(url)
+        return response.text()
+      }
+}
