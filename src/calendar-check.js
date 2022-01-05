168f469e8aa0775ca4ef79a3c020abeb28d15434
jacoobanderson <j00cwa@gmail.com>
2022-01-05 14:02:22 +0100

    Add JSDoc

--- src/calendar-check.js
+++ src/calendar-check.js
@@ -1,70 +1,91 @@
 import { JSDOM } from 'jsdom'
 import { LinkScraper } from './link-scraper.js'
 
+/**
+ * Checks the calendar for days that work for everyone.
+ */
 export class CalendarChecker {
-    async getFreeDays (url) {
-        const freeDays = []
-        const countDays = {}
-        const availableDays = []
-        const days = await this.#scrapeDays(url)
+  /**
+   * Checks the calendar for available days.
+   *
+   * @param {string} url - The calendar url.
+   * @returns {string[]} - List of available days.
+   */
+  async getFreeDays (url) {
+    const freeDays = []
+    const countDays = {}
+    const availableDays = []
+    const days = await this.#scrapeDays(url)
 
-        // Get all days that are OK.
-        for (let i = 0; i < days.length; i++) {
-                if (days[i][0] === 'ok') {
-                    freeDays.push('friday')
-                }
-                if (days[i][1] === 'ok') {
-                    freeDays.push('saturday')
-                }
-                if (days[i][2] === 'ok') {
-                    freeDays.push('sunday')
-                }
-        }
-
-        // Count each day
-        freeDays.forEach((x) => {
-            countDays[x] = (countDays[x] || 0) + 1
-        })
-
-        // Get the days that all friends are available
-        for (const key in countDays) {
-            if (countDays[key] === 3) {
-                availableDays.push(key)
-            }
-        }
-        return availableDays
+    // Get all days that are OK.
+    for (let i = 0; i < days.length; i++) {
+      if (days[i][0] === 'ok') {
+        freeDays.push('friday')
+      }
+      if (days[i][1] === 'ok') {
+        freeDays.push('saturday')
+      }
+      if (days[i][2] === 'ok') {
+        freeDays.push('sunday')
+      }
     }
 
-    async #scrapeDays (url) {
-        // Stores the information about which days are OK.
-        const data = []
+    // Count each day
+    freeDays.forEach((x) => {
+      countDays[x] = (countDays[x] || 0) + 1
+    })
 
-        // Gets the calendar links.
-        const links = await this.#getCalendarLinks(url)
-        const linkScraper = new LinkScraper()
-
-        // Loops through the links and gets the days which are OK.
-        for (let i = 0; i < links.length; i++) {
-            const dom = new JSDOM(await linkScraper.getInitialData(links[i]))
-            const days = Array.from(dom.window.document.querySelectorAll('td'))
-            .map(ele => ele.textContent.toLowerCase())
-            data.push(days)
-        }
-        return data
+    // Get the days that all friends are available
+    for (const key in countDays) {
+      if (countDays[key] === 3) {
+        availableDays.push(key)
+      }
     }
+    return availableDays
+  }
 
-    async #getCalendarLinks (url) {
-        const linkScraper = new LinkScraper()
-        const calendars = await linkScraper.getLinks(url)
+  /**
+   * Gets the days that are OK.
+   *
+   * @param {string} url - The calendar url.
+   * @returns {string[]} - List of days that are OK.
+   */
+  async #scrapeDays (url) {
+    // Stores the information about which days are OK.
+    const data = []
 
-        // Stores the links
-        const links = []
+    // Gets the calendar links.
+    const links = await this.#getCalendarLinks(url)
+    const linkScraper = new LinkScraper()
 
-        // Loops through each person's calendar and then adds the original URL with the specific calendar.
-         for (let i = 0; i < calendars.length; i++) {
-            const removeTwoLetters = calendars[i].substring(2)
-            links.push(url + removeTwoLetters)
-         }
-         return links
+    // Loops through the links and gets the days which are OK.
+    for (let i = 0; i < links.length; i++) {
+      const dom = new JSDOM(await linkScraper.getInitialData(links[i]))
+      const days = Array.from(dom.window.document.querySelectorAll('td'))
+        .map(ele => ele.textContent.toLowerCase())
+      data.push(days)
     }
+    return data
+  }
+
+  /**
+   * Gets the initial links.
+   *
+   * @param {string} url - the url that represents the calendar.
+   * @returns {string} The links.
+   */
+  async #getCalendarLinks (url) {
+    const linkScraper = new LinkScraper()
+    const calendars = await linkScraper.getLinks(url)
+
+    // Stores the links
+    const links = []
+
+    // Loops through each person's calendar and then adds the original URL with the specific calendar.
+    for (let i = 0; i < calendars.length; i++) {
+      const removeTwoLetters = calendars[i].substring(2)
+      links.push(url + removeTwoLetters)
+    }
+    return links
+  }
 }
--- src/reservation-checker.js
+++ src/reservation-checker.js
@@ -1,50 +1,73 @@
 import fetch from 'node-fetch'
 import { JSDOM } from 'jsdom'
 
+/**
+ * Represents the reservation class.
+ */
 export class ReservationChecker {
     #day
     #url
     #fetchCookie
-    constructor(url, day) {
-        this.#day = day
-        this.#url = url
-        this.#fetchCookie = undefined
+    /**
+     * Creates an instance of the reservation class.
+     *
+     * @param {string} url - string that represents the url of the dinner site.
+     * @param {string} day - the day that is to be checked.
+     */
+    constructor (url, day) {
+      this.#day = day
+      this.#url = url
+      this.#fetchCookie = undefined
     }
 
+    /**
+     * Gets the cookie from the response and puts it in the variable fetchCookie.
+     *
+     */
     async #getCookieFromResponse () {
-        const response = await fetch(`${this.#url}login`, {
-            headers: {
-                'content-type': 'application/x-www-form-urlencoded'
-              },
-              body: 'username=zeke&password=coys&submit=login',
-              method: 'POST',
-              redirect: 'manual'
-        })
-        this.#fetchCookie = response.headers.get('set-cookie').split(';')[0]
+      const response = await fetch(`${this.#url}login`, {
+        headers: {
+          'content-type': 'application/x-www-form-urlencoded'
+        },
+        body: 'username=zeke&password=coys&submit=login',
+        method: 'POST',
+        redirect: 'manual'
+      })
+      this.#fetchCookie = response.headers.get('set-cookie').split(';')[0]
     }
 
+    /**
+     * Gets the intitial booking data from the site by accessing it with the cookie.
+     *
+     * @returns {string} returns the html as text.
+     */
     async #getRestaurantBookingData () {
-        const response = await fetch(`${this.#url}login/booking`, {
-            headers: {
-                cookie: `${this.#fetchCookie}`
-            }
-        })
-        return response.text()
+      const response = await fetch(`${this.#url}login/booking`, {
+        headers: {
+          cookie: `${this.#fetchCookie}`
+        }
+      })
+      return response.text()
     }
 
-    async getBookingAlternatives() {
-        await this.#getCookieFromResponse()
-        const restaurantData = await this.#getRestaurantBookingData()
+    /**
+     * Compiles the available times.
+     *
+     * @returns {object} consists of what day and what time is available.
+     */
+    async getBookingAlternatives () {
+      await this.#getCookieFromResponse()
+      const restaurantData = await this.#getRestaurantBookingData()
 
-        const dom = new JSDOM(restaurantData)
-        const bookingTimes = Array.from(dom.window.document.querySelectorAll(`input[name="group1"][value^="${this.#day.slice(0, 3)}"] ~ span`))
+      const dom = new JSDOM(restaurantData)
+      const bookingTimes = Array.from(dom.window.document.querySelectorAll(`input[name="group1"][value^="${this.#day.slice(0, 3)}"] ~ span`))
         .map(ele => ele.textContent.trim().split(' ')[0])
-        
-        const compiledTimes = {
-            day: `${this.#day}`,
-            available: bookingTimes
-        }
-        
-        return compiledTimes
+
+      const compiledTimes = {
+        day: `${this.#day}`,
+        available: bookingTimes
+      }
+
+      return compiledTimes
     }
 }
